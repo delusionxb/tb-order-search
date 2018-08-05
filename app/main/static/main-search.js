40d170c78d9298df0f10bb44431e41a13e7c8bf2
Frank Xu <bing.xu@aqitrade.com>
2018-08-05 14:57:28 +0800

    add css and js files for main-search and top-bottom components main-search does not call functions to really load data yet top-bottom is good to go

---
+++ app/main/static/main-search.js
@@ -0,0 +1,222 @@
+
+let populateMainSearchContainer = function() {
+    log('main-search.populateMainSearchContainer()');
+    let mainSearchWrapper = `
+        <div class="main-search-wrapper">
+            <div class="base-search">
+                <div class="base-search-form">
+                    <input class="input is-info">
+                    <button class="button btn-search is-info">搜索</button>
+                    <button class="button btn-reset is-info">重置</button>
+                </div>
+                <div class="base-search-extension">
+                    <span>更多搜索条件</span>
+                    <span class="is-off">
+                        <i class="fas fa-angle-down"></i>
+                    </span>
+                    <span>
+                        <i class="fas fa-angle-up"></i>
+                    </span>
+                </div>
+                <div class="base-search-all">
+                    <button class="button btn-all is-info">所有订单</button>
+                </div>
+            </div>
+            <div class="condition-search-container">
+                <div class="condition-search-wrapper">
+                    <div class="condition-search-rows">
+                        <div class="condition-search-row">
+                            <div class="condition-search div totalCost">
+                                <span class="condition-search span">总价范围</span>
+                                <input class="condition-search input minTotalCost is-info">
+                                <input class="condition-search input maxTotalCost is-info">
+                            </div>
+                            <div class="condition-search div shopName">
+                                <span class="condition-search span">店铺名称</span>
+                                <input class="condition-search input is-info">
+                            </div>
+                            <div class="condition-search div orderType">
+                                <span class="condition-search span">订单类型</span>
+                                <div class="select is-info">
+                                  <select>
+                                    <option>所有</option>
+                                    <option>实物</option>
+                                    <option>虚拟</option>
+                                  </select>
+                                </div>
+                            </div>
+                            <div class="condition-search div orderStatus">
+                                <span class="condition-search span">订单状态</span>
+                                <div class="select is-info">
+                                  <select>
+                                    <option>所有</option>
+                                    <option>完成</option>
+                                    <option>﻿关闭</option>
+                                  </select>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="condition-search-row">
+                            <div class="condition-search div createDay">
+                                <div>
+                                    <span class="condition-search span">成交时间</span>
+                                </div>
+                                <div class="createDay-type select is-info">
+                                  <select class="createDay-type">
+                                    <option value="byMonth">按月</option>
+                                    <option value="customize">自选</option>
+                                  </select>
+                                </div>
+                                <div class="createDay-type-byMonth select is-info">
+                                  <select class="createDay-type-byMonth">
+                                    <option value="0">-- 自然月 --</option>
+                                    <option value="1">最近一个月</option>
+                                    <option value="3">最近三个月</option>
+                                    <option value="6">最近六个月</option>
+                                  </select>
+                                </div>
+                                <div class="createDay-type-byRange is-off">
+                                    <input type="date" class="condition-search input minCreateDay is-info"
+                                       min="2009-01-01" max="2100-12-31">
+                                    <input type="date" class="condition-search input maxCreateDay is-info"
+                                       min="2009-01-01" max="2100-12-31">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="condition-search-extension">TBD</div>
+                </div>
+            </div>
+        </div>
+    `;
+
+    $('.main-search-container').append($(mainSearchWrapper));
+};
+
+let toggleSearchExtension = function() {
+    log('main-search.toggleSearchExtension()');
+    $('.base-search-extension').click(function(event) {
+        $('.base-search-extension').find('span:nth-child(2), span:nth-child(3)').toggleClass('is-off');
+        $('.condition-search-container').toggle(400);
+    });
+};
+
+let populateSearchFields = function() {
+    log('main-search.populateSearchFields()');
+    let searchFields = {};
+    searchFields.itemName = $('.base-search-form>.input');
+    searchFields.minTotalCost = $('.condition-search.totalCost>.input.minTotalCost');
+    searchFields.maxTotalCost = $('.condition-search.totalCost>.input.maxTotalCost');
+    searchFields.shopName = $('.condition-search.shopName>.input');
+
+    let byMonthSelect = $('.createDay-type-byMonth');
+    let byRange = $('.createDay-type-byRange');
+    if (byMonthSelect.css('display') !== 'none') {
+        searchFields.byMonthOption = byMonthSelect.find('option:selected');
+    } else {
+        searchFields.minCreateDay = $('.createDay-type-byRange>.input.minCreateDay');
+        searchFields.maxCreateDay = $('.createDay-type-byRange>.input.maxCreateDay');
+    }
+    return searchFields;
+};
+
+// make 1-9 to '01'-'09'
+let plus0 = function(num) {
+    return num < 10 ? '0' + num : num;
+};
+
+let getSearchData = function() {
+    log('main-search.getSearchData()');
+    let searchFields = populateSearchFields();
+    let searchData = {};
+    for (let key in searchFields) {
+        if (searchFields.hasOwnProperty(key)) {
+            if (key === 'byMonthOption') {
+                let byMonthValue = searchFields.byMonthOption.val();
+                if (byMonthValue !== '0') {
+                    let now = new Date();
+                    let thisMonth = plus0(now.getMonth() + 1);
+                    let thatMonth = plus0(now.getMonth() + 1 - (byMonthValue - 1));
+                    let thisDay = plus0(now.getDate());
+                    searchData.minCreateDay = `${now.getFullYear()}-${thatMonth}-01`;
+                    searchData.maxCreateDay = `${now.getFullYear()}-${thisMonth}-${thisDay}`;
+                }
+            } else {
+                let conditionValue = searchFields[key].val();
+                if (conditionValue !== '') {
+                    searchData[key] = conditionValue;
+                }
+            }
+        }
+    }
+    log('searchData: ', searchData);
+    return searchData;
+};
+
+let resetSearchData = function() {
+    log('main-search.resetSearchData()');
+    let searchFields = populateSearchFields();
+    for (let key in searchFields) {
+        if (searchFields.hasOwnProperty(key) && key !== 'byMonthOption') {
+            // log('resetSearchData', key);
+            searchFields[key].val('');
+        }
+    }
+
+    $('.condition-search.orderType select>option:nth-child(1)').prop('selected', 'selected');
+    $('.condition-search.orderStatus select>option:nth-child(1)').prop('selected', 'selected');
+    $('select[class="createDay-type"]>option:nth-child(1)').prop('selected', 'selected');
+    let byMonthDiv = $('div.createDay-type-byMonth');
+    if (byMonthDiv.css('display') === 'none') {
+        byMonthDiv.toggle();
+        $('.createDay-type-byRange').toggle();
+    }
+    $('select[class="createDay-type-byMonth"]>option:nth-child(1)').prop('selected', 'selected');
+};
+
+let bindSearchFormBtns = function() {
+    $('.base-search>.input').keypress(function() {
+        log('main-search.bindSearchFormBtns() by Enter');
+        if (event.code === 'Enter') {
+            event.target.blur(); // make target lose focus
+            event.preventDefault(); // prevent default event
+            makePagination4Search(getSearchData());
+        }
+    });
+
+    $('.base-search-form>.button.btn-search').click(function(event) {
+        log('main-search.bindSearchFormBtns() by Conditions');
+        makePagination4Search(getSearchData());
+    });
+
+    $('.base-search-all>.button.btn-all').click(function(event) {
+        log('main-search.bindSearchFormBtns() by All');
+        resetSearchData();
+        makePagination();
+    });
+
+    $('.base-search-form>.button.btn-reset').click(function(event) {
+        log('main-search.bindSearchFormBtns() by Reset');
+        resetSearchData();
+    });
+};
+
+let toggleDateRangeType = function() {
+    log('main-search.toggleDateRangeType()');
+    let dateRangeType = $('select[class="createDay-type"]');
+    dateRangeType.change(function(event) {
+        $('div.createDay-type-byMonth').toggle(400);
+        $('.createDay-type-byRange').toggle(400);
+    });
+};
+
+let doSearch = function() {
+    toggleSearchExtension();
+    toggleDateRangeType();
+    bindSearchFormBtns();
+};
+
+
+populateMainSearchContainer();
+toggleSearchExtension();
+toggleDateRangeType();
---
+++ app/main/static/top-bottom.js
@@ -0,0 +1,35 @@
+
+let populateTopBottomContainer = function() {
+    log('top-bottom.populateTopBottomContainer()');
+    let topBottomWrapperHtml = `
+        <div class="top-bottom-wrapper">
+            <div class="top-bottom to-top">
+                <span>
+                    <i class="fas fa-arrow-alt-circle-up fa-3x"></i>
+                </span>
+            </div>
+            <div class="top-bottom to-bottom">
+                <span>
+                    <i class="fas fa-arrow-alt-circle-down fa-3x"></i>
+                </span>
+            </div>
+        </div>
+    `;
+
+    $('.top-bottom-container').append($(topBottomWrapperHtml));
+};
+
+let bindTopBottom = function() {
+    log('top-bottom.bindTopBottom()');
+    $('.top-bottom.to-top').click(function(event) {
+        window.scrollTo(0, 0);
+    });
+
+    $('.top-bottom.to-bottom').click(function(event) {
+        window.scrollTo(0, 100000);
+    });
+};
+
+
+populateTopBottomContainer();
+bindTopBottom();
